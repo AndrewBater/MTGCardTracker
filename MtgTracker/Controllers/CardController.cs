f1c77c7e5eb869f6b5c0acb4394cbb7608eeff51
Andrew Bater <andrewbater@googlemail.com>
2013-10-02 23:37:44 +0100

    Added to VS

---
+++ MtgTracker/Controllers/CardController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MtgTracker.Controllers
+{
+    public class CardController : Controller
+    {
+        //
+        // GET: /Card/
+
+        public ActionResult Index(string name)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MtgTracker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MtgTracker.MvcApplication" Language="C#" %>
