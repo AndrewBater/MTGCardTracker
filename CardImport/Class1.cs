25cc7f00db17f1e1f07f1796fbebeefd33ab42f5
Andrew Bater <andrewbater@googlemail.com>
2013-10-10 00:04:53 +0100

    Import json into database
    Problems with SQL generation, try parameters next time.

---
+++ CardImport/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MtgTracker.Models;
+using System.Web.Script.Serialization;
+using System.IO;
+using System.Data.SqlServerCe;
+using System.Data;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            SqlCeConnection connection = new SqlCeConnection("data source=D:\\Git\\MTGCardTracker\\Data\\MyDatabase#1.sdf");
+            connection.Open();
+            string setSQL = "INSERT INTO SETS(Name, Code, ReleaseDate, Border, Type, Block) VALUES ('{1}', '{2}', @ReleaseDate, '{3}', '{4}', '{5}')";
+            string cardSQL = "INSERT INTO CARD(Name, CMC, Rarity, Text, Flavor, Layout, MultiverseID, Printing, Names, ManaCost, Colours, SuperTypes, Types, SubTypes, Artist, Number, Power, Toughness, Loyalty, Variations, imageName, watermark, border, rulings, foreignNames, printings) VALUES ('{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}', '{10}', '{11}', '{12}', '{13}', '{14}', '{15}', '{16}', '{17}', '{18}', '{19}', '{20}', '{21}', '{22}', '{23}', '{24}', '{25}', '{26}')";
+            int cardID = 0;
+            int setID = 0;
+            var jsd = new JavaScriptSerializer();
+            string[] files = Directory.GetFiles("D:\\Git\\mtgjson\\json", "*.json", SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                setID++;
+                string json = new StreamReader(file).ReadToEnd();
+                Set set = jsd.Deserialize<Set>(json);
+                SqlCeParameter param = new SqlCeParameter("@ReleaseDate", SqlDbType.DateTime, 32, "ReleaseDate");
+                param.Value = set.releaseDate;
+                SqlCeCommand setCommand = new SqlCeCommand(String.Format(setSQL, setID, set.name, set.code, set.border, set.type, set.block));
+                setCommand.Parameters.Add(param);
+                setCommand.Connection = connection;
+                setCommand.ExecuteNonQuery();
+                foreach (Card card in set.cards)
+                {
+                    cardID++;
+                    
+                    SqlCeCommand cardCommand = new SqlCeCommand(String.Format(cardSQL,
+                        cardID, card.name.Replace("'", @"\'"), card.cmc, card.rarity, card.text != null ? card.text.Replace("'", @"\'") : null, card.flavor != null ? card.flavor.Replace("'", @"\'") : null, card.layout, card.multiverseid, card.printings, card.names, card.manaCost, card.colors, card.supertypes, card.types, card.subtypes, card.artist, card.number, card.power, card.toughness, card.loyalty, card.variations, card.imageName, card.watermark != null ? card.watermark.Replace("'", @"\'") : null, card.border, "", "", ""));
+                    
+                    cardCommand.Connection = connection;
+                    cardCommand.ExecuteNonQuery();
+                }
+            }
+            connection.Close();
+        }
+    }
+}
---
+++ MtgTracker/Models/SetModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MtgTracker.Models
+{
+    public class Set
+    {
+        public int setID { get; set; }
+        public string name { get; set; }
+        public string code { get; set; }
+        public DateTime releaseDate { get; set; }
+        public string border { get; set; }
+        public string type { get; set; }
+        public string block { get; set; }
+        public List<Card> cards { get; set; }
+    }
+
+    public class Printing
+    {
+        public int printingID { get; set; }
+        public int setID { get; set; }
+        public string cardID { get; set; }
+        public string artist { get; set; }
+    }
+
+    public class Card
+    {
+        public int cardID { get; set; }
+        public string layout { get; set; }
+        public string name { get; set; }
+        public string[] names { get; set; }
+        public string manaCost { get; set; }
+        public decimal cmc { get; set; }
+        public List<Colours> colors { get; set; }
+        public string[] supertypes { get; set; }
+        public string[] types { get; set; }
+        public string[] subtypes { get; set; }
+        public string rarity { get; set; }
+        public string text { get; set; }
+        public string flavor { get; set; }
+        public string artist { get; set; }
+        public string number { get; set; }
+        public string power { get; set; }
+        public string toughness { get; set; }
+        public string loyalty { get; set; }
+        public string multiverseid { get; set; }
+        public string[] variations { get; set; }
+        public string imageName { get; set; }
+        public string watermark { get; set; }
+        public string border { get; set; }
+        public List<Rulings> rulings { get; set; }
+        public List<ForeignNames> foreignNames { get; set; }
+        public string[] printings { get; set; }
+    }
+
+    public class Rulings
+    {
+        public DateTime date { get; set; }
+        public string text { get; set; }
+    }
+
+    public class ForeignNames
+    {
+        public string language { get; set; }
+        public string name { get; set; }
+    }
+
+    public enum Colours
+    {
+        White,
+        Blue,
+        Black,
+        Red,
+        Green,
+        Colorless
+    }
+}
